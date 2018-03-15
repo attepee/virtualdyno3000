33c3bd563641c22cfd877ff1d36a133bb1dffedc
Matti <mattiaaho1@gmail.com>
2018-03-15 16:47:49 +0200

    pohjustelua

---
+++ virtualdyno3000/newcarwindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace virtualdyno3000
+{
+    /// <summary>
+    /// Interaction logic for newcarwindow.xaml
+    /// </summary>
+    public partial class newcarwindow : Window
+    {
+        public newcarwindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
