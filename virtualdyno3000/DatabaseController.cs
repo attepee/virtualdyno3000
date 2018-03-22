eebc026a3621704afe70b4fde721276cd066252c
Matti644 <mattiaaho1@gmail.com>
2018-03-22 17:24:59 +0200

    sql haku luokka pohjaa

--- virtualdyno3000/DatabaseController.cs
+++ virtualdyno3000/DatabaseController.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace virtualdyno3000
 {
-    public static class DatabaseController
+    public static class DB
     {
+        static List<car> LoadCar()
+        {
+            List<car> cars = new List<car>();
+            return cars;
+        }
 
+        static bool CreateCar()
+        {
+            bool result = false;
+            return result;
+        }
+
+        static bool UpdateCar()
+        {
+            bool result = false;
+            return result;
+        }
+
+        static bool DeleteCar()
+        {
+            bool result = false;
+            return result;
+        }
+
+        static List<part> LoadPart()
+        {
+            List<part> parts = new List<part>();
+            return parts;
+        }
     }
 }
