8eb5da0ba44c9a60abff46ea3b098e70141eba9b
attepee <atte.pekkarinen@outlook.com>
2018-03-29 17:04:09 +0300

    Lisätty pari nappia pääikkunaan ja muokattu siinä olevien autojen tietojen esitystapaa

--- virtualdyno3000/MainWindow.xaml.cs
+++ virtualdyno3000/MainWindow.xaml.cs
@@ -26,10 +26,29 @@ namespace virtualdyno3000
             List<Car> cars = new List<Car>();
             cars = DB.LoadCar();
 
+            // Lisää stackpaneliin datagridin, näin datagridistä
+            // saa automaattisesti sopivan kokoisen riveihin nähden
+            DataGrid carGrid = new DataGrid();
+            stackPanel.Children.Add(carGrid);
             carGrid.ItemsSource = cars;
 
             //https://github.com/Microsoft/InteractiveDataDisplay.WPF
+        }
 
+        private void addCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Avaa uuden auton lisäämis ikkunan ja sulkee pääikkunan
+            NewCarWindow nCarWindow = new NewCarWindow();
+            nCarWindow.Show();
+            this.Close();
+        }
+
+        private void testCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Avaa auton dynottamis ikkunan ja sulkee pääikkunan
+            DynoWindow dWindow = new DynoWindow();
+            dWindow.Show();
+            this.Close();
         }
     }
 }
