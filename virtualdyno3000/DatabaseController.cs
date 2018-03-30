e0bd1697c62c6636f6bd0a6f295b42e507bcc210
Matti Aho <mattiaaho1@gmail.com>
2018-03-30 04:01:43 +0300

    sql haut tehty loppuun

--- virtualdyno3000/DatabaseController.cs
+++ virtualdyno3000/DatabaseController.cs
@@ -30,7 +30,7 @@ namespace virtualdyno3000
                 //creating query by requested id
                 if (id != 0)
                 {
-                    query = "SELECT " + id + " FROM cartable";
+                    query = "SELECT * FROM cartable where carId = " + id + ";";
                 }
                 else
                 {
@@ -44,7 +44,7 @@ namespace virtualdyno3000
                 while(dataReader.Read())
                 {
                     Car temp = new Car();
-                    temp.id = int.Parse(dataReader["idautot"].ToString());
+                    temp.id = int.Parse(dataReader["carId"].ToString());
                     temp.manufacturer = dataReader["manufacturer"].ToString();
                     temp.model = dataReader["model"].ToString();
                     temp.engine = int.Parse(dataReader["engine"].ToString());
@@ -72,7 +72,6 @@ namespace virtualdyno3000
         static bool CreateCar(Car c)
         {
             bool result = false;
-
             string query = string.Format("insert into cartable (manufacturer,model,engine,year,camshaft,piston,injectsystem,exhaust,turbo,block,broke) values('{0}','{1}',{2},{3},{4},{5},{6},{7},{8},{9},{10});", c.manufacturer, c.model, c.engine, c.year, c.camshaft, c.piston, c.injectionsystem, c.exhaust, c.turbo, c.block, c.broke);
 
             try
@@ -88,9 +87,10 @@ namespace virtualdyno3000
 
                 //get number of affected rows
                 int i = get.ExecuteNonQuery();
+                conn.Close();
 
                 //affected rows should be 1
-                if(i == 1)
+                if (i == 1)
                 {
                     result = true;
                 }
@@ -109,30 +109,130 @@ namespace virtualdyno3000
             return result;
         }
 
-        static bool UpdateCar()
+        static bool UpdateCar(Car c)
         {
             bool result = false;
-            return result;
-        }
+            string query = string.Format("update cartable set manufacturer = '{0}', model = '{1}', engine = {2}, year = {3}, camshaft = {4}, piston = {5}, injectsystem = {6}, exhaust = {7}, turbo = {8}, block = {9}, broke = {10} where carId = {11};", c.manufacturer, c.model, c.engine, c.year, c.camshaft, c.piston, c.injectionsystem, c.exhaust, c.turbo, c.block, c.broke, c.id);
 
-        static bool DeleteCar()
-        {
-            bool result = false;
-            return result;
-        }
-
-        static List<Part> LoadPart(int id)
-        {
-            List<Part> parts = new List<Part>();
             try
             {
-                conn.Open();
+                //check if connection is not already open
+                if (conn.State.ToString() == "Closed")
+                {
+                    conn.Open();
+                }
 
+                MySqlCommand get = new MySqlCommand(query, conn);
+                MySqlDataReader dataReader = get.ExecuteReader();
+
+                //get number of affected rows
+                int i = get.ExecuteNonQuery();
                 conn.Close();
+
+                //affected rows should be 1
+                if (i == 1)
+                {
+                    result = true;
+                }
+                else if (i > 1)
+                {
+                    //this shouldn't happen but ok
+                    throw new Exception("iso hälytys, liikaa rivejä muuttu");
+                }
             }
+
             catch (Exception ex)
             {
-                MessageBox.Show("Cannot open connection ! " + ex.ToString());
+                MessageBox.Show("Something wrong " + ex.ToString());
+            }
+
+            return result;
+        }
+
+        static bool DeleteCar(int id)
+        {
+            bool result = false;
+            string query = string.Format("delete from cartable where carId = {0};", id);
+
+            try
+            {
+                //check if connection is not already open
+                if (conn.State.ToString() == "Closed")
+                {
+                    conn.Open();
+                }
+
+                MySqlCommand get = new MySqlCommand(query, conn);
+                MySqlDataReader dataReader = get.ExecuteReader();
+
+                //get number of affected rows
+                int i = get.ExecuteNonQuery();
+                conn.Close();
+
+                //affected rows should be 1
+                if (i == 1)
+                {
+                    result = true;
+                }
+                else if (i > 1)
+                {
+                    //this shouldn't happen but ok
+                    throw new Exception("iso hälytys, liikaa rivejä muuttu");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something wrong " + ex.ToString());
+            }
+
+            return result;
+        }
+
+        static List<Part> LoadPart(int id = 0)
+        {
+            List<Part> parts = new List<Part>();
+            string query;
+
+            try
+            {
+                //check if connection is not already open
+                if (conn.State.ToString() == "Closed")
+                {
+                    conn.Open();
+                }
+
+                //creating query by requested id
+                if (id != 0)
+                {
+                    query = "SELECT * FROM tuningtable where partId = " + id + ";";
+                }
+                else
+                {
+                    query = "SELECT * FROM cartable";
+                }
+
+                MySqlCommand get = new MySqlCommand(query, conn);
+                MySqlDataReader dataReader = get.ExecuteReader();
+
+                //while reader has new data, keep creating new Car objects and add them to list
+                while (dataReader.Read())
+                {
+                    Part temp = new Part();
+                    temp.id = int.Parse(dataReader["partId"].ToString());
+                    temp.manufacturer = dataReader["manufacturer"].ToString();
+                    temp.partname = dataReader["partname"].ToString();
+                    temp.parttype = int.Parse(dataReader["parttype"].ToString());
+                    temp.stage = int.Parse(dataReader["stage"].ToString());
+                    temp.toughness = int.Parse(dataReader["toughness"].ToString());
+                    parts.Add(temp);
+                }
+                conn.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something wrong " + ex.ToString());
             }
 
             return parts;
