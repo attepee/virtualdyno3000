f23f8b13eafa1aae9003306cc61c0d5e05042cb9
Matti Aho <mattiaaho1@gmail.com>
2018-03-30 03:25:37 +0300

    auton luomiseen vaadittava toiminto luotu. koodia kommentoitu

--- virtualdyno3000/DatabaseController.cs
+++ virtualdyno3000/DatabaseController.cs
@@ -11,31 +11,36 @@ namespace virtualdyno3000
 {
     static class DB
     {
+        //create connection
         private static MySqlConnection conn = new MySqlConnection("SERVER=mysql.labranet.jamk.fi; DATABASE=K9251_3; UID=K9251;PASSWORD=glB9PN8Nn88ragKWgo4Q2d7YFd3mRrcS;");
 
         public static List<Car> LoadCar(int id = 0)
         {
             List<Car> cars = new List<Car>();
+            string query;
 
             try
             {
+                //check if connection is not already open
                 if (conn.State.ToString() == "Closed")
                 {
                     conn.Open();
                 }
 
-                string query = "SELECT * FROM cartable";
-
+                //creating query by requested id
                 if (id != 0)
                 {
                     query = "SELECT " + id + " FROM cartable";
                 }
-
-                List<string> result = new List<string>();
+                else
+                {
+                    query = "SELECT * FROM cartable";
+                }
 
                 MySqlCommand get = new MySqlCommand(query, conn);
                 MySqlDataReader dataReader = get.ExecuteReader();
 
+                //while reader has new data, keep creating new Car objects and add them to list
                 while(dataReader.Read())
                 {
                     Car temp = new Car();
@@ -53,39 +58,53 @@ namespace virtualdyno3000
                     temp.broke = int.Parse(dataReader["broke"].ToString());
                     cars.Add(temp);
                 }
+                conn.Close();
             }
+
             catch (Exception ex)
             {
-                MessageBox.Show("Cannot open connection ! " + ex.ToString());
+                MessageBox.Show("Something wrong " + ex.ToString());
             }
 
             return cars;
         }
 
-        static bool CreateCar(Car newCar)
+        static bool CreateCar(Car c)
         {
             bool result = false;
-            //insert into cartable (manufacturer,model,engine,year,camshaft,piston,injectsystem,exhaust,turbo,block,broke) values ('asd', 'sd', 1,1,1,1,1,1,1,1,1)
 
+            string query = string.Format("insert into cartable (manufacturer,model,engine,year,camshaft,piston,injectsystem,exhaust,turbo,block,broke) values('{0}','{1}',{2},{3},{4},{5},{6},{7},{8},{9},{10});", c.manufacturer, c.model, c.engine, c.year, c.camshaft, c.piston, c.injectionsystem, c.exhaust, c.turbo, c.block, c.broke);
 
-            /*
-                SqlCommand NewCmd = conn.CreateCommand();
-                NewCmd.Connection = conn;
-                NewCmd.CommandType = CommandType.Text;
-                NewCmd.CommandText = " update supplier set " + " ID = " + "'" + id + "'" + " , NAME = " + "'" + name + "'" + " , BALANCE = " + "'" + balance + "'" + " , PLACE = " + "'" + place + "'" + "  , LOCATION = " + "'" + address + "'" + ",  PHONE = " + "'" + phone + "'" + " , BANK_NAME = " + "'" + bankname + "'" + " , BANK_BRANCH = " + "'" + bankbranch + "'" + ", ACCOUNT_NO = " + "'" + accountno + "'" + " where ID = " + "@id";
-                NewCmd.Parameters.AddWithValue("@id", id);
-                int a = NewCmd.ExecuteNonQuery();
-                conn.Close();
-                if (a == 0)
+            try
+            {
+                //check if connection is not already open
+                if (conn.State.ToString() == "Closed")
                 {
-                    //Not updated.
+                    conn.Open();
                 }
 
-                else
-                {
+                MySqlCommand get = new MySqlCommand(query, conn);
+                MySqlDataReader dataReader = get.ExecuteReader();
 
+                //get number of affected rows
+                int i = get.ExecuteNonQuery();
+
+                //affected rows should be 1
+                if(i == 1)
+                {
+                    result = true;
                 }
-            */
+                else if (i > 1)
+                {
+                    //this shouldn't happen but ok
+                    throw new Exception("iso hälytys, liikaa rivejä muuttu");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something wrong " + ex.ToString());
+            }
 
             return result;
         }
