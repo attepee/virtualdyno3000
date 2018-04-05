7eff55bca0eda2074ec6e0edf5711c4b6d3d5b33
k9279 <k9279@student.jamk.fi>
2018-04-05 16:42:22 +0300

    dynosivunmuutokset

--- virtualdyno3000/dynowindow.xaml.cs
+++ virtualdyno3000/dynowindow.xaml.cs
@@ -23,5 +23,71 @@ namespace virtualdyno3000
         {
             InitializeComponent();
         }
+
+        private int ellipselenght = 374;
+        private Point currentposition = new Point(0, 228);
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int h = 0;
+            int a = 200;
+            int g = 0;
+            int f = 10;
+            for (int j = 0; j < 200; j++)
+            {
+                await Task.Delay(5);
+                Random r = new Random();
+                int rInt = r.Next(187, 189);
+                int homo = rInt - g;
+
+                //int range = 2;
+                Ellipse ellipsePoints = new Ellipse() { Width = 4, Height = 4, Fill = Brushes.Red };
+                Ellipse ellipsePoint = new Ellipse() { Width = 4, Height = 4, Fill = Brushes.Yellow };
+                Canvas.SetLeft(ellipsePoints, h);
+                Canvas.SetTop(ellipsePoints, homo);
+                Canvas.SetLeft(ellipsePoint, f);
+                Canvas.SetTop(ellipsePoint, homo);
+
+                myCanvas.Children.Add(ellipsePoint);
+
+                myCanvas.Children.Add(ellipsePoints);
+                h++;
+                h++;
+                a--;
+
+
+                g++;
+
+                f++;
+                f++;
+
+            }
+
+            Hp.Text = "HP: 250";
+            Nm.Text = "NM: 213";
+        }
+
+        private void Stopbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Savebutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menubutton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mWindow = new MainWindow();
+            mWindow.Show();
+            this.Close();
+        }
+
+        private void Tuning_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
